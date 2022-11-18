f4043c47d60f23d7358d2b002986205d9133641b
RomanPiat <108830304+RomanPiat@users.noreply.github.com>
2022-11-18 21:15:10 +0200

    hw 1 end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//console.log(galleryItems);
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGallery(items) {
+    return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+};
+
+const addCreateGallery = createGallery(galleryItems);
+
+gallery.innerHTML = addCreateGallery;
+
+gallery.addEventListener('click', modalWindowImage);
+
+function modalWindowImage(evt) {
+   evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}"width="800" height="600">`);
+    instance.show();
+    
+    gallery.addEventListener("keydown", (evt) => {
+        if (evt.code == "Escape") {
+          instance.close();
+        };
+    })
+};
